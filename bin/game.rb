2413870a5769352e444fd6871d84d027072fb655
Ethan Gould <ethan.g.907@gmail.com>
2014-04-28 11:54:24 -0400

    created a new game

---
+++ bin/game.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require_relative '../lib/person.rb'
+require_relative '../lib/player.rb'
+
+puts "Starting the game!"
+binding.pry
+
+jack = Person.new("Jack", "Sparrow")
+
+puts "Created #{jack.full_name}"
+
+game_man = Player.new("Matt", "Brendzel")
+puts "Game man's health is #{game_man.health}"
